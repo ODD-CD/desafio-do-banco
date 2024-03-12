0fa91d0cc7434a88e64c95bb2fd9fc7158319270
Lucas <lucas13dealmeidamoraes@gmail.com>
2024-03-12 17:29:47 -0300

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,12 @@
+
+public class Cliente {
+    public String nome;
+
+    public String geNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente();
+        cliente.setNome("Marcos");
+
+        Conta cc = new ContaCorrente(cliente);
+        cc.depositar(100);
+
+        Conta poupanca = new ContaPoupanca(cliente);
+
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
